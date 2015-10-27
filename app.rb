834541e9093c3803564f6e8064228aef6166e7cc
octavianrotari <octavianrotari@gmail.com>
2015-10-27 17:20:41 +0000

    cat-naming

--- app.rb
+++ app.rb
@@ -18,8 +18,13 @@ get '/random-cat' do
   erb :index
 end
 
-get '/named-cat' do
+post '/named-cat' do
   p params
   @name = params[:name]
   erb :index
+end
+
+get '/cat-naming' do
+  p params
+  erb :cat_form
 end
